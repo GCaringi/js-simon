68c566f0e6e512df8db6b6319b05f0cac1ac6625
GCaringi <giannicaringi94@gmail.com>
2022-05-12 16:59:57 +0200

    input with the html tag less a little bit of style

--- js/main.js
+++ js/main.js
@@ -38,6 +38,15 @@ function isInArray(num, arr){
     }
 }
 
+function strToNumArr(str){
+    str = str.split(" ");
+    let newArray = [];
+    for (let i = 0; i < str.length; i++){
+        newArray.push(Number(str[i]));
+    }
+    return newArray;
+}
+
 
 
 /*--------------------
@@ -57,7 +66,9 @@ function printNumber(section, size){
 --------------------*/
 
 const rndNumber = document.getElementById("rnd-num");
-const text = document.getElementById("input-text");
+const resultZone = document.getElementById("result");
+const submit = document.getElementById("btn");
+const form = document.getElementById("input-area");
 let newArr = generateRandomArr(5);
 
 console.log(newArr); // FOR DEBUG
@@ -66,18 +77,35 @@ printNumber(rndNumber, 5);
 
 setTimeout(function(){
     rndNumber.style.display = "none";
-    setTimeout(function(){
-        for (let i = 0; i < size; i++){
-            let numChooice = numIntegerPrompt();
-            if (isInArray(numChooice, newArr)){
-                point++;
+    console.log("sono passati 2s");
+    submit.addEventListener("click", 
+        function(){
+            let res = strToNumArr(document.getElementById("mychoice").value);
+            for (let i = 0; i < res.length; i++){
+                if (newArr.includes(res[i])){
+                    point++;
+                }
             }
+            if (point === newArr.length){
+                console.log("Hai vinto");
+                form.style.display = "none";
+                setTimeout(function(){
+                    resultZone.style.display = "flex";
+                }, 200)
+                resultZone.innerText = "You win!";
+            }else{
+                console.log("Hai perso");
+                form.style.display = "none";
+                setTimeout(function(){
+                    resultZone.style.display = "flex";
+                }, 200)
+                resultZone.innerText = "You lose!";
+            }
+            setTimeout(function(){
+                console.log("Sto ricaricando");
+                window.location.reload();
+            },2000);
         }
-        if (point === size){
-            text.innerText = "Hai vinto"
-        }else{
-            text.innerText = "Hai perso"
-        }
-    },100);
-}, 3000);
+    )
+}, 2000);
 
